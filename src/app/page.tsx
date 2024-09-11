ef4b099d2a708aaa21f13e4fea7e6c7ad209a943
MK <qscvdefb@gmail.com>
2024-09-12 01:28:47 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+const secretKey = process.env.REACT_APP_SECRET_KEY;
 
 export default function Home() {
   return (
     <div style={{fontSize: '150px'}}>
-        mẹ mày béo
+        {secretKey}
     </div>
   );
 }
