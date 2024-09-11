6c36259a3fe1ba89b787051d4364406de84b8f0c
MK <qscvdefb@gmail.com>
2024-09-12 01:32:44 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
-const secretKey = process.env.REACT_APP_SECRET_KEY;
+const secretKey = process.env.NEXT_PUBLIC_API_KEY;
 
 export default function Home() {
+  console.log(secretKey)
+
   return (
     <div style={{fontSize: '150px'}}>
         {secretKey}
